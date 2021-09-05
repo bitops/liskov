dc84f7def676002b389d2b3ea15ea74f649668a7
Sebastian Wittenkamp <sebastian@bitops.io>
2021-09-04 23:55:40 -0700

    Update timestamp.rb

--- timestamp.rb
+++ timestamp.rb
@@ -5,11 +5,6 @@ class Timestamp
 
   FORMAT = "%d/%b/%Y:%H:%M:%S"
 
-  #
-  # A note about this method. It is not the
-  # most efficient way of doing the conversion
-  # but it is quick and easy to implement.
-  #
   def self.from_log(date_str)
     # ex: 28/Jan/2012:08:34:41
     dt = DateTime.strptime(date_str, FORMAT)
